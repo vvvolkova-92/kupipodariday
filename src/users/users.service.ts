1d6931a7dc6c40ea74289be6a173922e0b172f53
vvvolkova-92 <saiviolet@gmail.com>
2023-04-12 22:56:13 +0300

    РЕВЬЮ / ЮЗЕР / Дополнительная обработка ошибок

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,8 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import {
+  ForbiddenException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { User } from './entities/user.entity';
@@ -17,8 +21,19 @@ export class UsersService {
   // создание пользователя
   async create(createUserDto: CreateUserDto): Promise<User> {
     const user = await this.userRepository.create(createUserDto);
+    const isUsernameTaken = await this.findQuery(createUserDto.username);
+    if (isUsernameTaken)
+      throw new ForbiddenException(
+        `Username ${createUserDto.username} занят, попробуйте другой`,
+      );
+    const isEmailTaken = await this.findQuery(createUserDto.email);
+    if (isEmailTaken)
+      throw new ForbiddenException(
+        `Пользователь с email ${createUserDto.email} уже зарегистрирован`,
+      );
     const { password, ...result } = user;
     const hashPassword = await bcrypt.hash(password, 10);
+
     return this.userRepository.save({ ...result, password: hashPassword });
   }
   // поиск всех пользователей
@@ -30,6 +45,15 @@ export class UsersService {
     const user = await this.userRepository.findOneBy({ id });
     if (!user) throw new NotFoundException(`Пользователь с id ${id} не найден`);
     return user;
+  }
+  //поисе по почте и юзеру для проверки
+  async findQuery(query): Promise<User[]> {
+    const user = await this.userRepository.find({
+      where: [{ email: query }, { username: query }],
+    });
+    if (user.length > 0) {
+      return user;
+    } else return undefined;
   }
   // поиск пользователя по username
   async findByUsername(username: string): Promise<User> {
@@ -58,7 +82,17 @@ export class UsersService {
     return wishes;
   }
   async update(id: number, updateUserDto: UpdateUserDto) {
-    const { password } = updateUserDto;
+    const { password, username, email } = updateUserDto;
+    const isUsernameTaken = await this.findQuery(username);
+    if (isUsernameTaken)
+      throw new ForbiddenException(
+        `Username ${username} занят, попробуйте другой`,
+      );
+    const isEmailTaken = await this.findQuery(email);
+    if (isEmailTaken)
+      throw new ForbiddenException(
+        `Пользователь с email ${email} уже зарегистрирован`,
+      );
     if (password) {
       const hashPassword = await bcrypt.hash(password, 10);
       const user = await this.userRepository.update(id, {
